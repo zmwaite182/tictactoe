201f9179ad707755bd016edbde7644237fe8a433
Zane Waite <zmwaite182@stevenscollege.edu>
2018-11-12 15:00:30 -0500

    Changed javascript to alternate between x's and o's

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 let cells = document.querySelectorAll('td');
 let cellsArray = Array.from(cells);
-console.log(cellsArray);
 
-for(let i = 0; i < cellsArray.length; i++) {
-  let cell = cellsArray[i];
-  cell.addEventListener('click', event => {
-    let letter = document.createTextNode('X');
-    cell.appendChild(letter);
-  });
+let p1 = 0;
+let p2 = 1;
+let turn = 0;
+
+if (turn < cellsArray.length) {
+  for(let i = 0; i < cellsArray.length; i++) {
+    let cell = cellsArray[i];
+    cell.addEventListener('click', event => {
+      if (turn % 2 == p1) {
+        let letter = document.createTextNode('X');
+        cell.appendChild(letter);
+      } else {
+        let letter = document.createTextNode('O');
+        cell.appendChild(letter);
+      }
+      turn++;
+    });
+  }
 }
